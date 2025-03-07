8f405981e26094dae2e19dc8793f6ad13da32930
Torben Bis <bis.torben@gmail.com>
2025-03-07 11:22:19 +0100

    add logic to close sidebar by clicking anywhere else

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Sidebar from "./components/Sidebar.jsx";
 function App() {
   return (
     <>
-      <h1>App</h1>
       <Sidebar></Sidebar>
     </>
   );
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,17 +1,34 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import "./Sidebar.css";
 
 const Sidebar = () => {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
+  const sidebarRef = useRef(null);
 
   const toggleSidebar = () => {
     setIsOpen(!isOpen);
-    console.log(isOpen);
   };
+
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (sidebarRef.current && !sidebarRef.current.contains(e.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    if (isOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
+    }
+
+    // useEffect cleanup function
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isOpen]);
 
   return (
     <>
-      <nav className={`Sidebar ${isOpen ? "open" : "closed"}`}>
+      <nav ref={sidebarRef} className={`Sidebar ${isOpen ? "open" : "closed"}`}>
         <button onClick={toggleSidebar}>on/off</button>
       </nav>
     </>
