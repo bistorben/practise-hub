9d9eae6fbcbf1466a258175cfae0a9d78740f8fd
Torben Bis <bis.torben@gmail.com>
2025-03-26 09:01:55 +0100

    wip: heroboschcom teaser

--- src/components/HeroBoschCom.jsx
+++ src/components/HeroBoschCom.jsx
@@ -1,6 +1,8 @@
 import "./HeroBoschCom.css";
 import { Link } from "react-router-dom";
 import { RxExternalLink } from "react-icons/rx";
+import heroImage from "../assets/HeroBoschCom/bosch_hero_big.webp";
+import heroLinkImage from "../assets/HeroBoschCom/bosch_hero_small.webp";
 
 const HeroBoschCom = () => {
   return (
@@ -17,6 +19,26 @@ const HeroBoschCom = () => {
           </span>
         </h1>
       </a>
+      <div className="hero-img-wrapper">
+        <div className="hero-img-big">
+          <img src={heroImage} alt="hero image" />
+        </div>
+
+        <div className="hero-link-list">
+          <img src={heroLinkImage} alt="link image" />
+          <ul>
+            <li>
+              <a href="">test123</a>
+            </li>
+            <li>
+              <a href="">test123</a>
+            </li>
+            <li>
+              <a href="">test123</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </section>
   );
 };
