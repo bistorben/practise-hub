cb6c6e1087884551378d9bceecbe77984ccb2897
Torben Bis <bis.torben@gmail.com>
2025-03-31 15:31:50 +0200

    wip: herBosch dynamic functions

--- src/components/edit/Edit.jsx
+++ src/components/edit/Edit.jsx
@@ -3,15 +3,81 @@ import { useState } from "react";
 
 const Edit = () => {
   const [label, setLabel] = useState("");
+  const [href, setHref] = useState("");
+  const [external, setExternal] = useState(false);
 
   const handleLabelChange = (e) => {
     setLabel(e.target.value);
   };
 
+  const handleHrefChange = (e) => {
+    setHref(e.target.value);
+  };
+
+  const handleExternalChange = (e) => {
+    setExternal(e.target.checked);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const newLink = {
+      label,
+      href,
+      external,
+    };
+
+    try {
+      const res = await fetch("http://localhost:5005/api/hero-bosch", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newLink),
+      });
+
+      const data = await res.json();
+      console.log("server response", data);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
   return (
     <section className="Edit">
       <h1>Edit components</h1>
-      <input type="text" value={label} onChange={handleLabelChange} />
+
+      <form className="hero-bosch-form" onSubmit={handleSubmit}>
+        <h4>Edit HeroBoschCom</h4>
+        <div className="form-group">
+          <label htmlFor="label">Link-Label:</label>
+          <input
+            type="text"
+            id="label"
+            value={label}
+            onChange={handleLabelChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="href">Link-Url</label>
+          <input
+            type="url"
+            id="href"
+            value={href}
+            onChange={handleHrefChange}
+          />
+        </div>
+        <div className="form-group checkbox-group">
+          <label htmlFor="external">External Link:</label>
+          <input
+            type="checkbox"
+            id="external"
+            checked={external}
+            onChange={handleExternalChange}
+          />
+        </div>
+        <button type="submit">save changes</button>
+      </form>
     </section>
   );
 };
